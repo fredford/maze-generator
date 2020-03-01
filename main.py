b6af978cccef88298e6abec459c61b70e1ab323a
fredford <fraser.redford@gmail.com>
2020-03-01 15:45:52 -0700

    generates mazes

--- main.py
+++ main.py
@@ -1,8 +1,81 @@
 import numpy as np
-#import pygame
 import maze_generator
-#import dfs_maze
+import pygame, time
 
 
-maze = maze_generator.Maze(10, 10)
-#maze = dfs_maze
+def main():
+    pygame.init()
+    board_width = 10
+    board_height = 10
+    screen = pygame.display.set_mode((board_width*50, board_height*50))
+    maze = maze_generator.Maze(board_width, board_height)
+    maze.print_maze()
+
+    cells = maze.maze
+
+    pygame.display.set_caption("Maze")
+    clock = pygame.time.Clock()
+
+
+    # Fill the background with white
+    screen.fill((255, 255, 255))
+
+    for i in range(len(cells)+1):
+        pygame.draw.line(screen, (0,0,0), (0,i*50), (board_width*50,i*50))
+
+    for j in range(len(cells[0])+1):
+        pygame.draw.line(screen, (0,0,0), (j*50,0), (j*50,board_height*50))
+
+    # Draw a solid blue circle in the center
+
+    cell = cells[0][0]
+    print(cell.walls)
+
+    pos_x = 0
+    pos_y = 0
+
+    pygame.Rect(pos_x, pos_y, pos_x+50, pos_y+50)
+
+    above = (pos_x + 5, pos_y, pos_x + 45, pos_y +45)
+    below = (pos_y + 5, pos_y + 5, pos_x + 45, pos_y + 50)
+    left = (pos_x, pos_y+5, pos_x+45, pos_y+45)
+    right = (pos_x+5, pos_y+5, pos_x+50, pos_y+45)
+
+    for i in range(len(cells)):
+        for j in range(len(cells[i])):
+
+            cell = cells[i][j]
+
+            for direction, value in cell.walls.items():
+
+                if not value and direction == "above":
+                    print('above')
+                    pygame.draw.line(screen, (255,255,255), ((50*i)+1, (50*j)) , ((50*i)+49, (50*j)))
+                if not value and direction == "below":
+                    print('below')
+                    pygame.draw.line(screen, (255,255,255), ((50*i)+1, (50*j)+50), ((50*i)+49, (50*j)+50))
+                if not value and direction == "left":
+                    print('left')
+                    pygame.draw.line(screen, (255,255,255), ((50*i), (50*j)), ((50*i), (50*j)+49))
+                if not value and direction == "right":
+                    print('right')
+                    pygame.draw.line(screen, (255,255,255), ((50*i)+50, (50*j)+1), ((50*i)+50, (50*j)+49))
+
+
+    # Flip the display
+    pygame.display.flip()
+
+    # Run until the user asks to quit
+    running = True
+    while running:
+
+        # Did the user click the window close button?
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    # Done! Time to quit.
+    pygame.quit()
+
+
+main()
--- maze_generator.py
+++ maze_generator.py
@@ -3,96 +3,68 @@ import numpy as np
 import random
 
 class Maze:
-
     def __init__(self, width, height):
 
+        self.directions = {"above":(0, -1), "below":(0, 1), "left":(-1, 0), "right":(1,0)}
         self.width = width
         self.height = height
         self.maze = []
         self.create_cells()
-        self.set_border()
         self.mesh_cells()
         self.dfs_make_maze()
-
-    def dfs_make_maze(self):
-
-        frontier = []
-
-        frontier.append(self.maze[1][1])
-
-        counter = 0
-
-        while len(frontier) > 0 and counter < 20:
-            self.print_maze()
-
-            random.shuffle(frontier)
-            #print(frontier)
-
-            if counter == 0:
-                current_cell = frontier.pop()
-                current_cell.set_path()
-
-            neighbors = self.valid_neighbors(current_cell)
-
-
-            if not neighbors:
-                current_cell = frontier.pop()
-                continue
-
-            next_cell = random.choice(neighbors)
-            next_cell.set_path()
-            frontier.append(current_cell)
-            current_cell = next_cell
-
-            counter += 1
-            print(counter)
-
-        self.print_maze()
-
-
-    def valid_neighbors(self, cell):
-        neighbors = []
-        if not cell.above.isBorder and cell.above.all_Walls():
-            neighbors.append(cell.above)
-        if not cell.below.isBorder and cell.below.all_Walls():
-            neighbors.append(cell.below)
-        if not cell.right.isBorder and cell.right.all_Walls():
-            neighbors.append(cell.right)
-        if not cell.left.isBorder and cell.left.all_Walls():
-            neighbors.append(cell.left)
-
-        return neighbors
-
 
     def create_cells(self):
         for i in range(self.width):
             temp = []
             for j in range(self.height):
                 temp.append(Cell(i, j))
-
             self.maze.append(temp)
 
-    def set_border(self):
-        for i in range(len(self.maze)):
-            for j in range(len(self.maze[i])):
-                if i == 0 or j == 0 or i == len(self.maze)-1 or j == len(self.maze[i])-1:
-                    cell = self.maze[i][j]
-                    cell.set_border()
-
     def mesh_cells(self):
-        for i in range(1,len(self.maze)-1):
-            for j in range(1,len(self.maze[i])-1):
-                cell = self.maze[i][j]
-                cell.set_cells(self.maze[i-1][j], self.maze[i+1][j], self.maze[i][j-1], self.maze[i][j+1])
+        for row in self.maze:
+            for cell in row:
+                for direction, (x,y) in self.directions.items():
+                    if cell.x + x < 0 or cell.y + y < 0:
+                        cell.neighbors[direction] = None
+                    elif cell.x + x == self.width or cell.y + y == self.height:
+                        cell.neighbors[direction] = None
+                    else:
+                        cell.neighbors[direction] = self.maze[cell.x + x][cell.y + y]
+
+    def valid_neighbors(self, cell):
+        neighbors = []
+
+        for direction in self.directions.keys():
+            if cell.neighbors[direction] != None and cell.neighbors[direction].all_walls():
+                neighbors.append(cell.neighbors[direction])
+
+        return neighbors
+
+    def dfs_make_maze(self):
+        current_cell = self.maze[0][0]
+        frontier = self.valid_neighbors(current_cell)
+        counter = 0
+
+        while len(frontier) > 0:
+            neighbors = self.valid_neighbors(current_cell)
+            counter += 1
+
+            if not neighbors:
+                current_cell = frontier.pop()
+                continue
+
+            next_cell = random.choice(neighbors)
+            current_cell.set_path(next_cell)
+            next_cell.set_path(current_cell)
+            frontier.append(current_cell)
+            current_cell = next_cell
 
     def print_maze(self):
-
         for i in range(len(self.maze)):
             line = ""
             for j in range(len(self.maze[i])):
                 line += str(self.maze[i][j])
             print(line)
-
 
 class Cell:
 
@@ -104,57 +76,28 @@ class Cell:
         self.below = None
         self.left = None
         self.right = None
-        self.isFilled = True
-        self.isStart = False
-        self.isEnd = False
-        self.isBorder = False
+        self.walls = {"above": True, "below": True, "left": True, "right": True}
+        self.neighbors = {}
+        self.test = False
 
-    def set_cells(self, above, below, left, right):
+    def all_walls(self):
+        return all(self.walls.values())
 
-        self.above = above
-        self.below = below
-        self.left = left
-        self.right = right
+    def set_path(self, cell):
+        for direction, neighbor in self.neighbors.items():
+            if neighbor != None:
+                if cell.x == neighbor.x and cell.y == neighbor.y:
+                    self.walls[direction] = False
 
-    def set_start(self):
-        self.isStart = True
-
-    def set_end(self):
-        self.isEnd = True
-
-    def set_path(self):
-        self.isFilled = False
-
-    def set_border(self):
-        self.isBorder = True
-        self.isFilled = False
-
-    def all_Walls(self):
-        self.get_neighbors()
-        return all(self.neighbors)
-
-    def get_neighbors(self):
-        neighbors = []
-
-        if not self.above.isBorder:
-            neighbors.append(self.above)
-        if not self.below.isBorder:
-            neighbors.append(self.below)
-        if not self.left.isBorder:
-            neighbors.append(self.left)
-        if not self.right.isBorder:
-            neighbors.append(self.right)
-
-        self.neighbors = neighbors
+    def __repr__(self):
+        return str(self)
 
     def __str__(self):
-        if self.isFilled:
-            return " 1"
-        if self.isBorder:
-            return " 4"
-        if self.isStart:
-            return " S"
-        if self.isEnd:
-            return " E"
+
+        if all(self.walls.values()):
+            return "("+str(self.x)+","+str(self.y)+","+str(sum(self.walls.values()))+")"
+            #return " 1"
         else:
-            return " 0"
+            return "("+str(self.x)+","+str(self.y)+","+str(sum(self.walls.values()))+")"
+            #return " 0"
+
